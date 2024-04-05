0465777d6de568d05707e3d68d90209c7c85f8fc
Logan Luna <LoganLambeth8@gmail.com>
2024-04-05 17:44:07 -0400

    Training!

--- YOLOV8Train.py
+++ YOLOV8Train.py
@@ -1,16 +1,28 @@
 from ultralytics import YOLO
 import wandb
 
-# Log in to wandb
-wandb.login(key="63033089206962899707825b6063aba50505b314")  # Replace with your wandb API key
-
-# Initialize wandb
-wandb.init(project="YoloV8__Combined_Aircraft", entity="logan03luna", name="Attempt1")
+import torch
+import torchvision
 
 
-# Load a model
-model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
-#model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 
-# Train the model
-results = model.train(data='CombinedDataset/data.yaml', epochs=100, imgsz=640)
+def setup_and_train():
+    print("PyTorch version:", torch.__version__)
+    print("torchvision version:", torchvision.__version__)
+    print("CUDA version:", torch.version.cuda)
+    print("CUDA is available:", torch.cuda.is_available())
+    print("CUDA device name:", torch.cuda.get_device_name(0))
+
+    # Check if a CUDA GPU is available and set PyTorch to use it
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f"Training on {device}")
+
+    # Load a model
+    model = YOLO('yolov8n.pt')  # Specify the device here
+
+    # Train the model
+    results = model.train(data='CombinedDataset/data.yaml', epochs=100, imgsz=640)
+    return results
+
+if __name__ == '__main__':
+    results = setup_and_train()
