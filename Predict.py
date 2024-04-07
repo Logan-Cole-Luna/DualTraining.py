45eb7563a52654f5bf60ec60cd79310df4ca3991
Logan Luna <LoganLambeth8@gmail.com>
2024-04-06 22:38:22 -0400

    Model successfully trained on combined datasets, it gained the ability to identify aircraft of all types as was the goal and civilian aircraft in a wide variety of settings. However, the civilian aircraft detection, though improved, does not achieve the desired accuracy

---
+++ Predict.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO('YoloV8CombinedAircraft/Attempt14/weights/best.pt')  # pretrained YOLOv8n model
+
+# Run batched inference on a list of images
+results = model(['img.png', 'CombinedDataset/train/images/1570250.jpg', 'img_1.png', 'img_2.png'])  # return a list of Results objects
+
+# Process results list
+for result in results:
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    masks = result.masks  # Masks object for segmentation masks outputs
+    keypoints = result.keypoints  # Keypoints object for pose outputs
+    probs = result.probs  # Probs object for classification outputs
+    result.show()  # display to screen
+    #result.save(filename='result.jpg')  # save to disk
---
+++ PredictVideo.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+
+# Load a pretrained YOLOv8n model
+model = YOLO('YoloV8CombinedAircraft/Attempt14/weights/best.pt')  # pretrained YOLOv8n model
+
+# Define source as YouTube video URL
+source = 'https://www.youtube.com/watch?v=UojjrMrW96g'
+
+# Run inference on the source
+results = model(source, stream=True)  # generator of Results objects
+# Process results list
+for result in results:
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    masks = result.masks  # Masks object for segmentation masks outputs
+    keypoints = result.keypoints  # Keypoints object for pose outputs
+    probs = result.probs  # Probs object for classification outputs
+    result.show()  # display to screen
+    #result.save(filename='result.jpg')  # save to disk
